231c9f99c62aaa69162b2303ec3620257382ad70
Samiul Sk <sam91v@gmail.com>
2017-04-10 23:34:04 +0530

    new

--- run_app.py
+++ run_app.py
@@ -5,7 +5,8 @@ import os
 #from library._02_html_inside_view import app
 #from library._03_template_str_inside_view import app
 #from library._04_template_outside_view import app
-from library._05_basic_routing import app
+#from library._05_basic_routing import app
+from library._06_raising_custom_error import app
 
 if __name__ == '__main__':
     app.debug = True

e070dc795a404dc51a9a4bfbaffe2421bd574f1a
Samiul Sk <sam91v@gmail.com>
2017-04-10 23:35:43 +0530

    to catch error, code added

--- library/_06_raising_custom_error.py
+++ library/_06_raising_custom_error.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, abort
+
 app = Flask(__name__)
+
 
 AUTHORS_INFO = {
     'poe': {
-        'full_name': 'Edgar Alan Poe',
+        'full_name': 'Edgar Allan Poe',
         'nationality': 'US',
         'notable_work': 'The Raven',
-        'born': 'January 19,1809',
+        'born': 'January 19, 1809',
         'picture': 'https://upload.wikimedia.org/wikipedia/commons/7/75/Edgar_Allan_Poe_2_retouched_and_transparent_bg.png'
     },
     'borges': {
@@ -15,7 +17,6 @@ AUTHORS_INFO = {
         'notable_work': 'The Aleph',
         'born': 'August 24, 1899',
         'picture': 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Jorge_Luis_Borges_1951%2C_by_Grete_Stern.jpg'
-
     }
 }
 
@@ -24,15 +25,19 @@ AUTHORS_INFO = {
 def authors():
     return render_template('routing/authors.html')
 
+
 @app.route('/author/<string:authors_last_name>')
 def author(authors_last_name):
     if authors_last_name not in AUTHORS_INFO:
         abort(404)
-    return render_template('routing/author.html',author=AUTHORS_INFO[authors_last_name])
+    return render_template('routing/author.html',
+                           author=AUTHORS_INFO[authors_last_name])
+
 
 @app.route('/author/<string:authors_last_name>/edit')
 def author_admin(authors_last_name):
     abort(401)
+
 
 @app.errorhandler(404)
 def not_found(error):

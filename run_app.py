176e5e0582acfde61631af20bcc9580e4fe1a580
Samiul Sk <sam91v@gmail.com>
2017-04-12 20:36:21 +0530

    modified:   .idea/workspace.xml 	new file:   library/_07_request_info.py 	new file:   library/templates/request/info.html 	modified:   run_app.py

--- run_app.py
+++ run_app.py
@@ -6,7 +6,8 @@ import os
 #from library._03_template_str_inside_view import app
 #from library._04_template_outside_view import app
 #from library._05_basic_routing import app
-from library._06_raising_custom_error import app
+#from library._06_raising_custom_error import app
+from library._07_request_info import  app
 
 if __name__ == '__main__':
     app.debug = True
